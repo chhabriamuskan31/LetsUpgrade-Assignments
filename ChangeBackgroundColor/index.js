370fb756263fdb039ecde246b17c80980e92c415
chhabriamuskan31 <muskanchhabria2002@gmail.com>
2023-01-13 20:24:33 +0530

    Changed background of paragraph

---
+++ ChangeBackgroundColor/index.js
@@ -0,0 +1,6 @@
+function color() {
+    const elems = document.getElementsByClassName('changecolor')
+    for (let i = 0; i < elems.length; i++) {
+        elems[i].style.backgroundColor = '#FAD3E7'
+    }
+};
